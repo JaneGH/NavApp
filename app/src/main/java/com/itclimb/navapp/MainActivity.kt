9515807a109239442c8ec382b6756186b5123e20
JaneGH <jkrytsyna@gmail.com>
2024-02-12 13:50:22 -0800

    Navigation in Jetpack

---
+++ app/src/main/java/com/itclimb/navapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.itclimb.navapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
